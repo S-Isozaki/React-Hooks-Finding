12391f0dc6275ab6e24e5aca661c67f0abd526f0
Isozaki Satoshi <i.satoshi1987@gmail.com>
2024-05-22 18:10:35 +0900

    add select element

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import Display from "./Display";
-import { useRef } from "react";
+import { useState, useRef } from "react";
 
 function App() {
-  const ref = useRef();
+  const displayRef = useRef();
+  const [num, setNum] = useState('25');
+  var str = generateRandomString(Number(num));
   return (
     <>
-      <Display ref={ref} />
-      <button onClick={() => {ref.current.drawString()}}>write</button>
-      <button onClick={() => {ref.current.clearCanvas()}}>clear</button>
+      <select onChange={e => setNum(e.target.value)}>
+        <option value="25">25</option>
+        <option value="50">50</option>
+        <option value="100">100</option>
+        <option value="200">200</option>
+      </select>
+      <Display ref={displayRef} />
+      <button onClick={() => {displayRef.current.drawString(str)}}>write</button>
+      <button onClick={() => {displayRef.current.clearCanvas()}}>clear</button>
     </>
   );
 };
+
+const generateRandomString = (len) => {
+  var c = "abcdefghijklmnopqrstuvwxyz0123456789";
+  
+  var cl = c.length;
+  var r = "";
+  for(var i=0; i<len; i++){
+      r += c[Math.floor(Math.random()*cl)];
+  }
+  return r;
+}
 
 export default App;
